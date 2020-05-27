12c1ca54539dad09bc69dc37eb0a31c6051de9d0
찬옥 <chanok9417@kakao.com>
2020-05-27 23:34:12 +0900

    Update scheduler.js

--- app/bootstrap/scheduler.js
+++ app/bootstrap/scheduler.js
@@ -10,16 +10,14 @@ const { timeStamp } = require('../common/util')
 exports.init = () => {
   // 매일 00:00:01 급식데이터 및 이번달 일정 데이터 갱신
   schedule.scheduleJob('1 0 0 * * * *', async () => {
-    console.log('급식데이터 및 이번달 일정 데이터 갱신합니다.')
     await Meal.update()
     await Calendar.update()
   })
 
   // 매 시간마다 날씨데이터 갱신
   schedule.scheduleJob('0 0 * * * * *', async () => {
-    console.log('학급 시간표와 날씨데이터 갱신합니다.')
-    await Weather.update()
     await Timetable.update()
+    await Weather.update()
   })
 
   console.log(timeStamp() + '스케줄러를 초기화 했어요.'.cyan)
