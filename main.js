e1e6200a23f5b75b11c8c7d5356f8d99d535980d
찬옥 <chanok9417@kakao.com>
2020-06-28 21:29:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const colors = require('colors')
 const { timeStamp } = require('./app/common/util')
 
 require('./app/bootstrap/init')(app, express).then(() => {
-  app.listen(process.env.PORT || 10000, () => console.log(timeStamp() + colors.rainbow('Buldang BAB Started, PORT: ' + app.get('port'))))
+  app.listen(process.env.PORT || 10000)
 }).catch(e => {
   console.log(timeStamp() + '서버 초기화 도중 오류가 발생했어요: ' + e.message.red)
 })
