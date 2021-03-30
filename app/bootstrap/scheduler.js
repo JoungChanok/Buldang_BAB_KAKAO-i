c3ac1153a1c5c1ab4a4deb6fb82b2109f060b172
정찬옥 <chanok9417@kakao.com>
2021-03-31 05:33:34 +0900

    210331-0533

--- app/bootstrap/scheduler.js
+++ app/bootstrap/scheduler.js
@@ -8,13 +8,7 @@ const { timeStamp } = require('../common/util')
 
 exports.init = () => {
   // 매일 00:00:01 급식데이터 및 이번달 일정 데이터 갱신
-  schedule.scheduleJob('1 0 0 * * * *', async () => {
-    // await Meal.update()
-    // await Calendar.update()
-  })
-
-  // 매 시간마다 날씨데이터 갱신
-  schedule.scheduleJob('0 0 * * * * *', async () => {
+  schedule.scheduleJob('1 0 0 * * *', async () => {
     await Meal.update()
     await Calendar.update()
   })
